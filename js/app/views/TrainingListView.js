0fbc2f60b342346afc53ab7aca77d51464bc4ba1
Peng Yu <joshyupeng@gmail.com>
2014-08-01 23:52:18 +0800

    ttt

--- js/app/views/TrainingListView.js
+++ js/app/views/TrainingListView.js
@@ -35,32 +35,34 @@ define([
 
         markdone: function(e){
             e.preventDefault();
-            
-            bootbox.prompt('please input training code:', function  (res) {
-                if( res !== null ){
-                    debugger;
+
+            bootbox.prompt('please input training code:', function  (trcode) {
+                if( trcode !== null ){
+                    var data = {formData: {trcode : trcode}}
+                    app.modelHelper.get('campus_training').execute('campus:events:training:checktrcode',{data: data}, success: function (stat) {
+                        if(stat){
+                            bootbox.confirm("Training code is correct. Are you sure to mark it done ?", function (res) {
+                                if(res){
+                                    var item= e.target;
+                                    var itemId = $(item).data('item-id');
+                                    var data = {formData: {id : itemId , trcode : trcode } };
+
+                                    app.modelHelper.get('campus_training').execute('campus:events:training:markdone',{data: data, success: function(status){
+                                         if(status){
+                                            var $grp = $(item).parents('.btn-group:first');
+                                            $grp.fadeOut('slow', function(){
+                                                var $urlDom = $grp.children().first().html('open').css('opacity', 0);
+                                                $grp.replaceWith($urlDom[0]);
+                                                $urlDom.animate({opacity:1}, 'slow');
+                                            });
+                                         }
+                                    }});
+                                }
+                            })
+                        }
+                    })
                 }
-            });
-
-
-            if(!trcode) return false;
-
-            debugger;
-
-            var item= e.target;
-            var itemId = $(item).data('item-id');
-            var data = {formData: {id : itemId , trcode : trcode } };
-
-            app.modelHelper.get('campus_training').execute('campus:events:training:markdone',{data: data, success: function(status){
-                 if(status){
-                    var $grp = $(item).parents('.btn-group:first');
-                    $grp.fadeOut('slow', function(){
-                        var $urlDom = $grp.children().first().html('open').css('opacity', 0);
-                        $grp.replaceWith($urlDom[0]);
-                        $urlDom.animate({opacity:1}, 'slow');
-                    });
-                 }
-            }});
+            });            
         }
     });
 });

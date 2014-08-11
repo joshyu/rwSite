02ec1b9eb65265a6688ee6652d1f0b38c47f0fbc
U-RA-INT\JYu5 <JYu5@APCNDAE-B7DDG02.ra-int.com>
2014-08-11 09:21:39 +0800

    clear all

--- js/app/app.js
+++
@@ -1,7 +0,0 @@
-define(['jquery', 'backbone', 'marionette', 'underscore'],
-    function($, Backbone, Marionette, _) {
-        var app = new Marionette.Application();
-        window.app = app;
-        return app;
-    }
-);
--- todo.md
+++
@@ -1,2 +0,0 @@
-1. More than 3 employees whose birthday is in current month.
-ssssss
